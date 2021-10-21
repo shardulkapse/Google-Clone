6681384574bb0f3b8a3d89c9b0a8d42ff5e63560
shardulkapse <56304874+shardulkapse@users.noreply.github.com>
2021-10-21 19:13:51 +0530

    Update Avatar.js

--- components/Avatar.js
+++ components/Avatar.js
@@ -4,7 +4,7 @@ function Avatar({ url, className }) {
       loading="lazy"
       src={url}
       alt="Profile Picture"
-      className={`${className} rounded-full h-7 w-7 cursor-pointer transition duration-250 transform hover:scale-110`}
+      className={`${className} rounded-full h-7 w-7 cursor-pointer transition duration-250 transform hover:scale-110 hidden sm:inline-flex`}
     />
   );
 }
